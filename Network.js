35b76b89796e19c8a09f42484b9cabe7dde306a0
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2021-03-16 20:50:29 +0300

    Update Network.js

--- Network.js
+++ Network.js
@@ -55,7 +55,7 @@ module.exports = class Network
             console.log('listening on *:' + this.port);
         });
     }
-    SetEvents(client)
+    SetEvents = (client) =>
     {
         this.events += {
             disconnect: data => {
