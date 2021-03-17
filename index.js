0936f8faab411119ae02273470947a4c276115a1
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2021-03-17 17:39:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ network.onConnect = client => {
 network.onRoomStarted = room => {
   console.log("hellfdsgfdfs");
   const colors = ["white", "black"].shuffle();
-  room.players[0].send("color", colors[0]);
-  room.players[1].send("color", colors[1]);
+  room.players[0].send("GameColor", colors[0], 1);
+  room.players[1].send("GameColor", colors[1], 1);
 };
 
 network.onGameStarted = room => {
