e96afd852c22eb37060fbabe1e05915854a7b060
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2021-03-17 08:16:30 +0300

    Update Network.js

--- Network.js
+++ Network.js
@@ -70,6 +70,7 @@ module.exports = class Network
                 this.rooms.JoinRandom(client);
             },
         };
+        console.log(this.events);
         for (const name in this.events) {
             var callback = this.events[name];
             if(typeof callback === "function")
